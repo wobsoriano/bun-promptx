4226e28fd82dd05db1f703523c21e789f01f734d
Robert Soriano <sorianorobertc@gmail.com>
2022-07-19 14:56:47 -0700

    update example

--- playground.ts
+++ playground.ts
@@ -1,9 +1,13 @@
 import { createSelection } from "./src";
 
 const result = createSelection([
+  { text: "feat", description: "Introducing new features" },
   { text: "fix", description: "Bug fix" },
   { text: "docs", description: "Writing docs" },
-  { text: "style", description: "Improving structure/format of the code" }
-])
+  { text: "style", description: "Improving structure/format of the code" },
+  { text: "refactor", description: "Refactoring code" }
+], {
+  headerText: 'Select Commit Type: '
+})
 
 console.log(result)
