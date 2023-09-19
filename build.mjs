909765f79c6a2101b48b296c2e8b9653b88e8cd3
wobsoriano <sorianorobertc@gmail.com>
2023-09-18 20:52:52 -0700

    chore: use bun build

---
+++ build.mjs
@@ -0,0 +1,29 @@
+import dts from 'bun-plugin-dts'
+import path from 'node:path'
+
+const output = await Bun.build({
+  entrypoints: ['./src/index.ts'],
+  outdir: './dist',
+  external: ['bun:ffi'],
+  plugins: [
+    dts()
+  ],
+})
+
+
+const XGO = path.join(process.env.HOME, 'go/bin/xgo');
+const TARGETS = 'linux/arm64,linux/amd64,darwin/arm64,darwin/amd64';
+
+if (output.success) {
+  console.log('Compiling native binaries...')
+  const proc = Bun.spawnSync([
+    XGO,
+    "-go", "1.20.3",
+    "-out", "release/promptx",
+    `--targets=${TARGETS}`,
+    "-ldflags=-s -w",
+    "-buildmode=c-shared",
+    ".",
+  ]);
+  console.log(proc.stdout.toString())
+}
